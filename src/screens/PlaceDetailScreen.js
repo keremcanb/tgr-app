67f66267ae1cc5a2398b0ace6a27658893618eef
Keremcan Buyuktaskin <keremcan@gmail.com>
2020-05-05 00:14:09 +0300

    Reverted back to params

--- src/screens/PlaceDetailScreen.js
+++ src/screens/PlaceDetailScreen.js
@@ -11,112 +11,102 @@ import {
 } from 'react-native';
 import MapView from 'react-native-maps';
 import { MarkdownView } from 'react-native-markdown-view';
-import useResources from '../components/useResources';
-
-// Before your getPlaces effect resolves, places will be an empty array, so place = places.find(...) will return undefined.
-
-// The issue is with the place, before getPlaces complete, the places is an empty array and .find method returns undefined. Accessing an image prop on place undefined throws an error. Perform null check before accessing the image prop on place.
 
 const PlaceDetailScreen = ({ navigation }) => {
-  const places = useResources('places');
-  const selectedPlace = navigation.getParam('placeId');
-  const place = places.find((item) => item._id === selectedPlace);
-  const bookingUrl = `${place && place.link}`;
+  const placesTitle = navigation.getParam('placeTitle');
+  const placesImage = navigation.getParam('placeImage');
+  const placesContent = navigation.getParam('placeContent');
+  const placesInfo = navigation.getParam('placeInfo');
+  const placesLink = navigation.getParam('placeLink');
+  const placesLat = navigation.getParam('placeLat');
+  const placesLng = navigation.getParam('placeLng');
+
   const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+  const latLng = `${placesLat},${placesLng}`;
   const label = 'Custom Label';
-  const latLng = `${place && place.lat},${
-    place && place.lng
-  }`;
   const url = Platform.select({
     ios: `${scheme}${label}@${latLng}`,
     android: `${scheme}${latLng}(${label})`,
   });
 
+  const bookingUrl = `${placesLink}`;
+
   return (
     <ScrollView>
-      {place && (
-        <>
-          <Image source={{ uri: place.image }} style={styles.image} />
+      <Image source={{ uri: placesImage }} style={styles.image} />
 
-          <View>
-            {!!place.link && (
-              <View style={styles.buttonTop}>
-                <Button
-                  title="REZERVASYON"
-                  color="#2a1a73"
-                  onPress={() => Linking.openURL(bookingUrl)}
-                />
-              </View>
-            )}
+      <View>
+        {!!placesLink && (
+          <View style={styles.buttonTop}>
+            <Button
+              title="REZERVASYON"
+              color="#2a1a73"
+              onPress={() => Linking.openURL(bookingUrl)}
+            />
           </View>
+        )}
+      </View>
 
-          <View style={styles.container}>
-            <MarkdownView styles={markdownStyles}>
-              {place.content}
-            </MarkdownView>
+      <View style={styles.container}>
+        <MarkdownView styles={markdownStyles}>{placesContent}</MarkdownView>
+      </View>
+
+      <View>
+        {!!placesInfo && (
+          <>
+            <Text style={styles.heading3}>Bilgiler</Text>
+            <View style={styles.line} />
+            <Text style={styles.container}>{placesInfo}</Text>
+          </>
+        )}
+      </View>
+
+      <View>
+        {!!placesLink && (
+          <View style={styles.buttonBottom}>
+            <Button
+              title="REZERVASYON"
+              color="#2a1a73"
+              onPress={() => Linking.openURL(bookingUrl)}
+            />
           </View>
+        )}
+      </View>
 
-          <View>
-            {!!place.info && (
-              <>
-                <Text style={styles.heading3}>Bilgiler</Text>
-                <View style={styles.line} />
-                <Text style={styles.container}>{place.info}</Text>
-              </>
-            )}
-          </View>
+      <View>
+        {!!placesLat && !!placesLat && (
+          <>
+            <Text style={styles.heading3}>Harita</Text>
+            <View style={styles.line} />
+            <MapView
+              showsUserLocation
+              style={styles.map}
+              initialRegion={{
+                latitude: placesLat,
+                longitude: placesLng,
+                latitudeDelta: 0.0022,
+                longitudeDelta: 0.0121,
+              }}
+            >
+              <MapView.Marker
+                coordinate={{ latitude: placesLat, longitude: placesLng }}
+                title={placesTitle}
+                onPress={() => Linking.openURL(url)}
+              />
+            </MapView>
+          </>
+        )}
+      </View>
 
-          <View>
-            {!!place.link && (
-              <View style={styles.buttonBottom}>
-                <Button
-                  title="REZERVASYON"
-                  color="#2a1a73"
-                  onPress={() => Linking.openURL(bookingUrl)}
-                />
-              </View>
-            )}
-          </View>
-
-          <View>
-            {!!place.lat && !!place.lng && (
-              <>
-                <Text style={styles.heading3}>Harita</Text>
-                <View style={styles.line} />
-                <MapView
-                  showsUserLocation
-                  style={styles.map}
-                  initialRegion={{
-                    latitude: place.lat,
-                    longitude: place.lng,
-                    latitudeDelta: 0.0022,
-                    longitudeDelta: 0.0121,
-                  }}
-                >
-                  <MapView.Marker
-                    coordinate={{
-                      latitude: place.lat,
-                      longitude: place.lng,
-                    }}
-                    title={place.title}
-                    onPress={() => Linking.openURL(url)}
-                  />
-                </MapView>
-              </>
-            )}
-          </View>
-
-        </>
-      )}
     </ScrollView>
   );
 };
 
 PlaceDetailScreen.navigationOptions = (navData) => {
-  const placeTitle = navData.navigation.getParam('placeTitle');
+  const place = navData.navigation.getParam('placeTitle');
 
   return {
-    headerTitle: placeTitle,
+    headerTitle: place,
   };
 };
 
@@ -144,20 +134,6 @@ const styles = StyleSheet.create({
     marginLeft: '4%',
     marginRight: '4%',
     marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#2a1a73',
-    padding: 6,
-    borderRadius: 3,
-    marginLeft: '30%',
-    marginRight: '30%',
-    marginTop: 15,
-  },
-  buttonText: {
-    fontSize: 17,
-    color: '#fff',
-    textAlign: 'center',
-    letterSpacing: 1,
   },
   buttonTop: {
     marginLeft: '30%',
@@ -189,16 +165,16 @@ const markdownStyles = {
   heading3: {
     fontSize: 20,
     fontFamily: 'nunito-bold',
-    marginBottom: 10,
     textTransform: 'lowercase',
     textTransform: 'capitalize',
+    marginBottom: 10,
   },
   heading4: {
     fontSize: 18,
     fontFamily: 'nunito-bold',
-    marginBottom: 10,
     textTransform: 'lowercase',
     textTransform: 'capitalize',
+    marginBottom: 10,
   },
   imageWrapper: {
     padding: 4,
